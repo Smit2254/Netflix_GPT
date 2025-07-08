51129e21d08c3938f1086e34698b69a20a7dc5e1
Smit Bhavsar <smitbhavsar23@gmail.com>
2025-07-08 11:50:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <p className='text-3xl font-bold text-red-300'>Namaste Everyone, Lets Make Nexflix GPT</p>
+    </>
+  );
+}
+
+export default App;
